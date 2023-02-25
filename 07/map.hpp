fe6fcc8e1818b8e0a2057bf642e0b3f0638674fa
Ivan Betev <iabetev@edu.hse.ru>
2023-02-25 18:37:13 +0300

    Created 07 hw

---
+++ 07/map.hpp
@@ -0,0 +1,468 @@
+#pragma once
+#include <functional>
+#include <tuple>
+
+namespace avl {
+
+    template <typename value_type>
+    struct Node {
+
+        Node(const value_type& pair) : m_pair(pair) {}
+        value_type m_pair;
+        Node* m_parent = nullptr;
+        Node* m_left = nullptr;
+        Node* m_right = nullptr;
+        size_t m_height = 1;
+    };
+
+    template<
+        class Key,
+        class Value,
+        class Comparator = std::less<Key>,
+        class Allocator = std::allocator<Node<std::pair<const Key, Value>>>
+    >
+	class MyMap {
+    public:
+        using key_type = Key;
+        using mapped_type = Value;
+        using key_compare = Comparator;
+        using value_type = std::pair<const Key, Value>;
+    public:
+        using Node = Node<value_type>;
+        class const_iterator;
+        class iterator;
+        class const_reverse_iterator;
+        class reverse_iterator;
+
+
+        MyMap() = default;
+        ~MyMap() { delete_tree(m_root); }
+
+        iterator begin() { return iterator{ find_min(m_root) }; }
+        iterator end() { return iterator(); }
+        const_iterator begin() const { const_iterator{ find_min(m_root) }; }
+        const_iterator end() const { return const_iterator{}; }
+        const_iterator cbegin() const { return begin(); }
+        const_iterator cend() const { return end(); }
+
+        reverse_iterator rbegin() { return reverse_iterator{ find_max(m_root) }; }
+        reverse_iterator rend() { return reverse_iterator{}; }
+        const_reverse_iterator rbegin() const { return const_reverse_iterator{ find_max(m_root) }; }
+        const_reverse_iterator rend() const { return const_reverse_iterator{}; }
+        const_reverse_iterator crbegin() const { return const_reverse_iterator{ find_max(m_root) }; }
+        const_reverse_iterator crend() const { return const_reverse_iterator{}; }
+
+        std::tuple<iterator, bool> insert(const value_type& value);
+        size_t erase(const Key& key);
+        iterator find(const Key& key) { return iterator{ find_(m_root, key) }; }
+        bool contains(const Key& key) const { return find_(m_root, key) != nullptr; }
+        bool empty() const noexcept { return !m_root; }
+        size_t size() const noexcept { return m_size; }
+        void clear();
+        mapped_type& operator[](const Key& key);
+        mapped_type& at(const Key& key);
+        const mapped_type& at(const Key& key) const { return at(key); }
+
+    private:
+        size_t height(Node* p) const noexcept { return p ? p->m_height : 0; }
+        int balance_factor(Node* p) const noexcept { return height(p->m_right) - height(p->m_left); }
+        void fixheight(Node* p);
+        Node* rotate_right(Node* p);
+        Node* rotate_left(Node* q);
+        Node* balance(Node* p);
+        Node* find_min(Node* p) { return p->m_left ? find_min(p->m_left) : p; }
+        Node* remove_min(Node* p);
+        Node* insert_(Node* p, const value_type& key);
+        Node* remove_(Node* p, const Key& key);
+        Node* find_(Node* p, const Key& key) const;
+        void delete_tree(Node* p);
+        Node* find_max(Node* p) { return p->m_right ? find_max(p->m_right) : p; }
+    private:
+        Node* m_root = nullptr;
+        bool m_success_insert = false;
+        Node* m_insert_it = nullptr;
+        bool m_success_remove = false;
+        size_t m_size = 0;
+        Allocator allocator;
+	};
+
+
+
+    template<class Key,class Value,class Comparator,class Allocator>
+    class MyMap<Key, Value, Comparator, Allocator>::const_iterator {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        // using value_type = std::pair<const Key, Value>; 
+        using difference_type = typename size_t;
+        using pointer = const value_type*;
+        using reference = const value_type&;
+        
+        const_iterator() = default;
+        const_iterator(Node* it) : m_it(it) {}
+
+        const_iterator& operator++();
+        const_iterator operator++(int);
+        const_iterator& operator--();
+        const_iterator operator--(int);
+        reference operator*() const { return m_it->m_pair; }
+        pointer operator->() const { return &(m_it->m_pair); }
+        bool operator==(const_iterator it) const { return m_it == it.m_it; }
+        bool operator!=(const_iterator it) const { return !(*this == it); }
+
+    private:
+        Node* m_it = nullptr;
+    };
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    class MyMap<Key, Value, Comparator, Allocator>::iterator : public const_iterator {
+    public:
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        iterator& operator++();
+        iterator operator++(int);
+        iterator& operator--();
+        iterator operator--(int);
+        reference operator*();
+        pointer operator->();
+
+    };
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    class MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator : public const_iterator {
+    public:
+        const_reverse_iterator& operator++();
+        const_reverse_iterator operator++(int);
+        const_reverse_iterator& operator--();
+        const_reverse_iterator operator--(int);
+    };
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    class MyMap<Key, Value, Comparator, Allocator>::reverse_iterator : public iterator {
+    public:
+        reverse_iterator& operator++();
+        reverse_iterator operator++(int);
+        reverse_iterator& operator--();
+        reverse_iterator operator--(int);
+    };
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    std::tuple<typename MyMap<Key, Value, Comparator, Allocator>::iterator, bool> MyMap<Key, Value, Comparator, Allocator>::insert(const value_type& value) {
+        m_root = insert_(m_root, value);
+        if (m_success_insert) ++m_size;
+        return std::make_tuple(iterator(m_insert_it), m_success_insert);
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    size_t MyMap<Key, Value, Comparator, Allocator>::erase(const Key& key) {
+        m_root = remove_(m_root, key);
+        if (m_success_remove) {
+            --m_size;
+            return 1;
+        }
+        return 0;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    void MyMap<Key, Value, Comparator, Allocator>::clear() {
+        delete_tree(m_root);
+        m_root = nullptr;
+        m_size = 0;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::mapped_type& MyMap<Key, Value, Comparator, Allocator>::operator[](const Key& key) {
+        Node* p = find_(m_root, key);
+        if (p) return p->m_pair.second;
+        m_root = insert_(m_root, { key, Value() });
+        ++m_size;
+        return m_insert_it->m_pair.second;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::mapped_type& MyMap<Key, Value, Comparator, Allocator>::at(const Key& key) {
+        Node* p = find_(m_root, key);
+        if (p) return p->m_pair.second;
+        throw std::out_of_range("Accessing a non-existent Key");
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    void MyMap<Key, Value, Comparator, Allocator>::fixheight(Node* p) {
+        size_t left_h = height(p->m_left);
+        size_t right_h = height(p->m_right);
+        p->m_height = 1 + (left_h > right_h ? left_h : right_h);
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::rotate_right(Node* p) {
+        Node* q = p->m_left;
+        q->m_parent = p->m_parent;
+        p->m_parent = q;
+        if (q->m_right)	q->m_right->m_parent = p;
+        p->m_left = q->m_right;
+        q->m_right = p;
+        fixheight(p);
+        fixheight(q);
+        return q;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::rotate_left(Node* q) {
+        Node* p = q->m_right;
+        p->m_parent = q->m_parent;
+        q->m_parent = p;
+        if (p->m_left) p->m_left->m_parent = q;
+        q->m_right = p->m_left;
+        p->m_left = q;
+        fixheight(p);
+        fixheight(q);
+        return p;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::balance(Node* p) {
+        fixheight(p);
+        if (balance_factor(p) == 2) {
+            if (balance_factor(p->m_right) < 0) {
+                p->m_right = rotate_right(p->m_right);
+                if (p->m_right) p->m_right->m_parent = p;
+            }
+            return rotate_left(p);
+        }
+        if (balance_factor(p) == -2) {
+            if (balance_factor(p->m_left) > 0) {
+                p->m_left = rotate_left(p->m_left);
+                if (p->m_left) p->m_left->m_parent = p;
+            }
+            return rotate_right(p);
+        }
+        return p;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::remove_min(Node* p) {
+        if (!p->m_left) {
+            // delete p->m_left;
+            allocator.deallocate(p->m_left, 1);
+            return p->m_right;
+        }
+        p->m_left = remove_min(p->m_left);
+        if (p->m_left) p->m_left->m_parent = p;
+        return balance(p);
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::insert_(Node* p, const value_type& key) {
+        if (!p) {
+            m_success_insert = true;
+            // Node* node = new Node(key);
+            Node* node = allocator.allocate(1);
+            new(node) Node(key);
+            m_insert_it = node;
+            return node;
+        }
+        if(key.first == p->m_pair.first) {
+            m_success_insert = false;
+            m_insert_it = p;
+        }
+        else if (key_compare{}(key.first, p->m_pair.first)) {
+            p->m_left = insert_(p->m_left, key);
+            p->m_left->m_parent = p;
+        }
+        else {
+            p->m_right = insert_(p->m_right, key);
+            p->m_right->m_parent = p;
+        }
+        
+        return balance(p);
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::remove_(Node* p, const Key& key) {
+        if (!p) {
+            m_success_remove = false;
+            return nullptr;
+        }
+        if (key == p->m_pair.first) {
+            m_success_remove = true;
+            Node* q = p->m_left;
+            Node* r = p->m_right;
+            auto parent = p->m_parent;
+            // delete p;
+            allocator.deallocate(p, 1);
+            if (!r) return q;
+            Node* min = find_min(r);
+            min->m_parent = parent;
+            min->m_right = remove_min(r);
+            if (min->m_right) min->m_right->m_parent = min;
+            min->m_left = q;
+            if (min->m_left) min->m_left->m_parent = min;
+            return balance(min);
+        }
+        else if (key_compare{}(key, p->m_pair.first)) {
+            p->m_left = remove_(p->m_left, key);
+            if (p->m_left) p->m_left->m_parent = p;
+        }
+        else {
+            p->m_right = remove_(p->m_right, key);
+            if (p->m_right) p->m_right->m_parent = p;
+        }
+        
+        return balance(p);
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::Node* MyMap<Key, Value, Comparator, Allocator>::find_(Node* p, const Key& key) const {
+        if (!p) return nullptr;
+        if (p->m_pair.first == key) return p;
+        else if (key_compare{}(key, p->m_pair.first)) {
+            if (p->m_left) return find_(p->m_left, key);
+            return nullptr;
+        }
+        if (p->m_right) return find_(p->m_right, key);
+        return nullptr;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    void MyMap<Key, Value, Comparator, Allocator>::delete_tree(Node* p) {
+        if (!p) return;
+        delete_tree(p->m_left);
+        delete_tree(p->m_right);
+        // delete p;
+        allocator.deallocate(p, 1);
+    }
+
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_iterator& MyMap<Key, Value, Comparator, Allocator>::const_iterator::operator++() {
+        if (m_it->m_right) {
+            m_it = m_it->m_right;
+            while (m_it->m_left) m_it = m_it->m_left;
+            return *this;
+        }
+        auto parent = m_it->m_parent;
+        while (parent && parent->m_left != m_it) {
+            m_it = parent;
+            parent = m_it->m_parent;
+        }
+        m_it = parent;
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_iterator MyMap<Key, Value, Comparator, Allocator>::const_iterator::operator++(int) {
+        const_iterator tmp = *this;
+        this->operator++();
+        return tmp;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_iterator& MyMap<Key, Value, Comparator, Allocator>::const_iterator::operator--() {
+        if (m_it->m_left) {
+            m_it = m_it->m_left;
+            while (m_it->m_right) m_it = m_it->m_right;
+            return *this;
+        }
+        auto parent = m_it->m_parent;
+        while (parent && parent->m_right != m_it) {
+            m_it = parent;
+            parent = m_it->m_parent;
+        }
+        m_it = parent;
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_iterator MyMap<Key, Value, Comparator, Allocator>::const_iterator::operator--(int) {
+        const_iterator tmp = *this;
+        this->operator--();
+        return tmp;
+    }
+
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::iterator& MyMap<Key, Value, Comparator, Allocator>::iterator::operator++() {
+        const_iterator::operator++();
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::iterator MyMap<Key, Value, Comparator, Allocator>::iterator::operator++(int) {
+        iterator tmp = *this;
+        const_iterator::operator++();
+        return tmp;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::iterator& MyMap<Key, Value, Comparator, Allocator>::iterator::operator--() {
+        const_iterator::operator--();
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::iterator MyMap<Key, Value, Comparator, Allocator>::iterator::operator--(int) {
+        iterator tmp = *this;
+        const_iterator::operator--();
+        return tmp;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::iterator::reference MyMap<Key, Value, Comparator, Allocator>::iterator::operator*() {
+        return const_cast<reference>(const_iterator::operator*());
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::iterator::pointer MyMap<Key, Value, Comparator, Allocator>::iterator::operator->() {
+        return const_cast<pointer>(const_iterator::operator->());
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator& MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator::operator++() {
+        const_iterator::operator--();
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator::operator++(int) {
+        const_iterator::operator--(1);
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator& MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator::operator--() {
+        const_iterator::operator++();
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator MyMap<Key, Value, Comparator, Allocator>::const_reverse_iterator::operator--(int) {
+        const_iterator::operator++(1);
+        return *this;
+    }
+
+    // nvfqehbfq
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::reverse_iterator& MyMap<Key, Value, Comparator, Allocator>::reverse_iterator::operator++() {
+        iterator::operator--();
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::reverse_iterator MyMap<Key, Value, Comparator, Allocator>::reverse_iterator::operator++(int) {
+        iterator::operator--(1);
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::reverse_iterator& MyMap<Key, Value, Comparator, Allocator>::reverse_iterator::operator--() {
+        iterator::operator++();
+        return *this;
+    }
+
+    template<class Key, class Value, class Comparator, class Allocator>
+    MyMap<Key, Value, Comparator, Allocator>::reverse_iterator MyMap<Key, Value, Comparator, Allocator>::reverse_iterator::operator--(int) {
+        iterator::operator++(1);
+        return *this;
+    }
+
+}
---
+++ 07/test.cpp
@@ -0,0 +1,231 @@
+#include "gtest/gtest.h"
+#include "map.hpp"
+#include <map>
+#include <string>
+#include <vector>
+#include "allocator.hpp">
+
+
+TEST(MapTest, InsertTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i) {
+		auto [it, success] = m.insert({ i, values[i] });
+		if (!success)
+			FAIL();
+	}
+
+	// ключ 2 уже есть в m => вставки не будет
+	auto [it, success] = m.insert({ 2, "e"});
+	if (success)
+		FAIL();
+
+	std::vector<mymap::value_type> target = { {0, "a"}, {1, "b"}, {2, "c"}, {3, "d"} };
+	std::vector<mymap::value_type> result;
+	// работаспособность range-based for цикла доказывает корректность моих итераторов
+	for (auto val : m)
+		result.push_back(val);
+	ASSERT_EQ(result, target);
+}
+
+TEST(MapTest, IteratorsTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i) 
+		auto [it, success] = m.insert({ i, values[i] });
+
+	ASSERT_EQ(m.begin()->second, "a");
+	ASSERT_EQ(m.rbegin()->second, "d");
+
+	// итерирование слева направо было проверено в прошлом тесте, теперь будет справа налево
+	std::vector<mymap::value_type> target = { {3, "d"}, {2, "c"}, {1, "b"}, {0, "a"} };
+	std::vector<mymap::value_type> result;
+	for (auto it = m.rbegin(); it != m.rend(); ++it)
+		result.push_back(*it);
+	ASSERT_EQ(result, target);
+}
+
+
+TEST(MapTest, EraseTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		auto [it, success] = m.insert({ i, values[i] });
+
+	size_t success = m.erase(10);
+	if (success)
+		FAIL(); // ключа 10 нет 
+
+	success = m.erase(2);
+	if (!success)
+		FAIL(); // ключ 2 есть
+
+	success = m.erase(2);
+	if (success)
+		FAIL(); // ключа 2 уже нет
+
+	std::vector<mymap::value_type> target = { {0, "a"}, {1, "b"}, {3, "d"} };
+	std::vector<mymap::value_type> result;
+	for (auto val : m)
+		result.push_back(val);
+	ASSERT_EQ(result, target);
+}
+
+
+TEST(MapTest, FindTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		auto [it, success] = m.insert({ i, values[i] });
+
+	mymap::iterator it = m.find(5);
+	if (it != m.end())
+		FAIL(); // ключа 5 нет
+
+	it = m.find(1);
+	if (it == m.end())
+		FAIL(); // ключ 1 есть
+
+	ASSERT_EQ(it->second, "b");
+}
+
+
+
+TEST(MapTest, ContainsTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		auto [it, success] = m.insert({ i, values[i] });
+
+
+	ASSERT_EQ(m.contains(10), false);
+	ASSERT_EQ(m.contains(2), true);
+}
+
+
+TEST(MapTest, EmptyTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+
+	ASSERT_EQ(m.empty(), true);
+
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		auto [it, success] = m.insert({ i, values[i] });
+
+	ASSERT_EQ(m.empty(), false);
+}
+
+
+
+TEST(MapTest, SizeTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i) {
+		auto [it, success] = m.insert({ i, values[i] });
+		ASSERT_EQ(m.size(), i + 1);
+	}
+
+	m.erase(0);
+	ASSERT_EQ(m.size(), 3);
+}
+
+
+
+TEST(MapTest, ClearTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		auto [it, success] = m.insert({ i, values[i] });
+
+	m.clear();
+	ASSERT_EQ(m.empty(), true);
+}
+
+
+TEST(MapTest, OperatorIndexTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		m[i] = values[i];
+
+	std::vector<mymap::value_type> target = { {0, "a"}, {1, "b"}, {2, "c"}, {3, "d"} };
+	std::vector<mymap::value_type> result;
+	for (auto val : m)
+		result.push_back(val);
+	ASSERT_EQ(result, target);
+
+	m[1] = "B";
+
+	ASSERT_EQ(m[1], "B");
+}
+
+
+
+TEST(MapTest, AtTest) {
+	using mymap = avl::MyMap<int, std::string>;
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		m[i] = values[i];
+
+	ASSERT_EQ(m.at(2), "c");
+
+	try {
+		m.at(10);
+	}
+	catch (std::out_of_range&) {
+		SUCCEED();
+	}
+	catch (...) {
+		FAIL();
+	}
+}
+
+
+
+TEST(MapTest, CompareTest) {
+	using mymap = avl::MyMap<int, std::string, std::greater<int>>; // !!!
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		m[i] = values[i];
+
+	std::vector<mymap::value_type> target = { {3, "d"}, {2, "c"}, {1, "b"}, {0, "a"} };
+	std::vector<mymap::value_type> result;
+	for (auto el: m)
+		result.push_back(el);
+	ASSERT_EQ(result, target);
+}
+
+
+
+TEST(MapTest, AllocatorTest) {
+	using mymap = avl::MyMap<int, std::string, std::less<int>,
+		myalloc::allocator<avl::Node<std::pair<const int, std::string>>>>; // !!!
+	mymap m;
+	std::vector<std::string> values = { "a", "b", "c", "d" };
+	for (int i = 0; i < 4; ++i)
+		m[i] = values[i];
+
+	std::vector<mymap::value_type> target = { {0, "a"}, {1, "b"}, {2, "c"}, {3, "d"} };
+	std::vector<mymap::value_type> result;
+	for (auto val : m)
+		result.push_back(val);
+	ASSERT_EQ(result, target);
+}
+
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
